a80deb7d8d112f69b4a91c2545a25a270629f8f8
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-10-04 19:11:29 +0200

    custom post type

--- wp-content/themes/Newtheme/functions.php
+++ wp-content/themes/Newtheme/functions.php
@@ -97,6 +97,15 @@
             'hierarchical'=> true,
             'show_admin_column'=> true,
         ]);
+        register_post_type('bien',[
+            'label'=> 'Bien',
+            'public'=> true,
+            'menu_position' => 3,
+            'menu_icon'=> 'dashicons-building',
+            'supports' => ['title', 'editor', 'thumbnail'],
+            'show_in_rest' => true,
+            'has_archive'=> true,
+        ]);
     }
 
     add_action('init', 'newtheme_init');
