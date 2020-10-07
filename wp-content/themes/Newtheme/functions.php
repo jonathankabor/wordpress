e1e018ae6f62820c98620a7f703b12a99af72552
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-10-07 09:44:51 +0200

    API customize

--- wp-content/themes/Newtheme/functions.php
+++ wp-content/themes/Newtheme/functions.php
@@ -1,6 +1,9 @@
     <?php
 
 
+    require_once ('options/apparence.php');
+
+
     function newtheme_supports (){
         add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
@@ -17,8 +20,10 @@
         wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', []);
         wp_register_script('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
         wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', [], false, true);
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
+        if (!is_customize_preview()){
+            wp_deregister_script('jquery');
+            wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
+        }
         wp_enqueue_style('bootstrap');
         wp_enqueue_script('bootstrap');
     }
--- wp-content/themes/Newtheme/header.php
+++ wp-content/themes/Newtheme/header.php
@@ -9,8 +9,11 @@
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-    <a class="navbar-brand" href="#"><?php bloginfo('name')?></a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4"
+     style="background-color: <?= get_theme_mod('header_background'); ?>!important;">
+    <a class="navbar-brand" href="#">
+        <?php bloginfo('name')?>
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
